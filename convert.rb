7f5a09f2c3fb8caa11a3f50123a8550181d31e82
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-02-07 20:29:24 -0800

    whew, we've got _some_ semi-similar information now, and we can access both gh-issues and ticgit

---
+++ convert.rb
@@ -0,0 +1,45 @@
+require 'ticgit'
+require 'octopi'
+
+class Issue
+	# from ticgit
+	#attr_accessor :id, :name, :title, :state, :milestone, :assigned
+	#attr_accessor :opened, :points
+	#attr_accessor :comments, :tags, :attachments #arrays
+	
+	# from octopi
+	#attr_accessor :repository, :user, :updated_at, :votes, :number, :title
+	#attr_accessor :body, :closed_at, :labels, :state, :created_at
+	
+	attr_accessor :id, :title
+	
+	def to_s
+		return "#{@id}: #{@title}"
+	end
+end
+
+user = 'xiongchiamiov'
+project = 'synchronizer-test'
+issues = []
+
+repo = Octopi::Repository.find(:user => user, :name => project)
+
+repo.issues.each do |gh_issue|
+	issue = Issue.new
+	issue.id = gh_issue.number
+	issue.title = gh_issue.title
+	
+	issues << issue
+end
+
+ticgit = TicGit.open('.')
+
+ticgit.ticket_list.each do |ti_issue|
+	issue = Issue.new
+	issue.id = ti_issue.ticket_id
+	issue.title = ti_issue.ticket_name
+	
+	issues << issue
+end
+
+issues.each { |issue| puts issue }
